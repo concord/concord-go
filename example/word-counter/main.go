f26c260b50e3c731b47abbb1ba1bbe350c89de84
Igor Vasilcovsky <vasilcovsky@gmail.com>
2015-10-24 18:45:45 -0400

    manifest for word_counter example

--- example/word-counter/main.go
+++ example/word-counter/main.go
@@ -27,6 +27,11 @@ func (c *Computation) ProcessRecords(ctx *concord.Context, r *concord.Record) er
 		c.dict[k] = 0
 	}
 	c.dict[k] += 1
+
+	if c.pidx%1024 == 0 {
+		log.Println(c.dict)
+	}
+
 	return nil
 }
 
@@ -43,32 +48,3 @@ func main() {
 	}
 	log.Fatal(concord.Serve(comp))
 }
-
-// class WordCounter(Computation):
-//     def __init__(self):
-//         self.dict = {}
-//         self.pidx = 0 # print index
-
-//     def init(self, ctx):
-//         self.concord_logger.info("Counter initialized")
-
-//     def process_timer(self, ctx, key, time):
-//         raise Exception('process_timer not implemented')
-
-//     def process_record(self, ctx, record):
-//         self.pidx += 1
-//         if self.dict.has_key(record.key):
-//             self.dict[record.key] += 1
-//         else:
-//             self.dict[record.key] = 1
-
-//         if (self.pidx % 1024) == 0:
-//             self.concord_logger.info(self.dict)
-
-//     def metadata(self):
-//         return Metadata(
-//             name='word-counter',
-//             istreams=['words'],
-//             ostreams=[])
-
-// serve_computation(WordCounter())
